46d19559e733c4c5d08c78acc60f8b0278c6c622
Michael Ramón <michael.ramon@ucuenca.edu.ec>
2023-10-20 20:32:11 -0500

    Se creó un código para que una pelota se mueva de izquierda hasta el borde de la pantalla y aparezca nuevamente desde el lado opuesto y que cada que pase esto se cambie el color de fondo. Se modifico las instrucciones del Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# michael-ramón_pelota
+# Pelota que rebota
+
+Este es el primer proyecto de la clase de código creativo de la Universidad de Cuenca. Se trata de una pelota que rebota.
+
+Está programado con la librería de JavaScript p5.js
--- sketch.js
+++ sketch.js
@@ -1,7 +1,44 @@
+let posX = 10;
+let posY = 10;
+let diametro;
+let radio;
+let fondo;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  // print(posX);
+  // posX = 234;
+  // print(posX);
+  // posX = posX + 25;
+  // print(posX);
+  // posX = posX * 2;
+  // print(posX);
+  // posX = posX / 5;
+  // print(posX);
+  // posX *= 8;
+  // print(posX);
+  // posX+= 1;
+  // print(posX);
+  // posX ++;
+  // print(posX);
+  posX = windowWidth / 2;
+  posY = windowHeight / 2;
+  diametro = 100;
+  radio = diametro / 2;
+  fondo = color(25, 100, 50);
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+  background(fondo);
+  fill(255);
+  circle(posX, posY, diametro);
+  posX -= 10;
+
+  if (posX < -radio) {
+    fondo = color(255, random(100, 175), 50);
+    diametro = random(10, 100);
+    radio = diametro / 2;
+    posX = windowWidth + radio;
+  }
+  print(posX);
 }
